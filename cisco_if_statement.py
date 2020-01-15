665cc3cb8727a223f8d2fafeb75b11ad27680cdb
Thomas <thomasleroy@protonmail.com>
2020-01-15 15:43:01 +0100

    new function about if

--- cisco_if_statement.py
+++ cisco_if_statement.py
@@ -1,10 +1,19 @@
-ages =[1,10,60,22,55,44,77,99,20,33,49,31,21,28,27,63]
-
-def compare_age():
-    for numbers in ages:
-        if numbers < 20:
+import time
+"""
+def compare_age(aList):
+    for numbers in aList:
+        #time.sleep(1)
+        if numbers <= 20:
             print("small")
         else:
             print("large")
-compare_age()
 
+list_ages =[100,10,5,20,55,44,77,99,20,33,49,31,21,28,27,63]
+compare_age(list_ages)
+
+"""
+name_list=["test", "testmaisautre"]
+for i in range(len(name_list)):
+    print(i)
+    print(name_list[i])
+    
