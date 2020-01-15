628c08165187500f370db5fccb66ea7f61eec336
Thomas <thomasleroy@protonmail.com>
2020-01-15 14:35:05 +0100

    additionnal files

---
+++ function.py
@@ -0,0 +1,5 @@
+def print_arie(): 
+    name="arie"
+    return(name)
+
+print_arie()
--- loops.py
+++ loops.py
@@ -1,2 +1,3 @@
-for x in range(0, 3):
-    print("We're on time %d" % (x))
+for x in range(1, 11):
+    for y in range(1, 11):
+        print('%d * %d = %d' % (x, y, x*y))
