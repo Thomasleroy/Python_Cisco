07cb1ad5ef258c37b667eda7aa3fe0a3e288d653
Thomas <thomasleroy@protonmail.com>
2020-01-15 14:13:17 +0100

    update

--- first.py
+++ first.py
@@ -1,4 +1,13 @@
+import pandas as pd
+
 names = ["arie", "thomas","Koen"]
 
-name_dict = {"name": names}
-print(name_dict)
+names_list =["arie","thomas"]
+ages_list =[29, 32]
+people_dict = {"name" : names_list, 
+               "age": ages_list}
+
+#print("this is the dico \n{} \n".format(people_dict))
+
+personen_df = pd.DataFrame(people_dict)
+print(personen_df)
