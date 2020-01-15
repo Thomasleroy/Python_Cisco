1c5704b5937391fe3d56a374e4b894a0d09ec805
Thomas <thomasleroy@protonmail.com>
2020-01-15 14:58:18 +0100

    function

--- function.py
+++ function.py
@@ -1,5 +1,29 @@
+from math import pi
+
 def print_arie(): 
     name="arie"
-    return(name)
+    print(name)
 
 print_arie()
+
+#2nd function
+'''
+arguments:
+
+'''
+def name_extra(name,name_wife):
+    print("hello {}".format(name))
+    print("how is your wife {} ?".format(name_wife))
+
+name_extra("thomas","TEST")
+
+"""
+comment can be done this way 
+"""
+
+def calculate_circle(radius):
+    surface = (radius * radius) * pi
+    return surface, pi
+
+surface_circle= calculate_circle(5)
+print("{}".format(surface_circle))
